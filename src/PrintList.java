ce033749230b03733bc9c16a046b2b6e55bffa91
Aarohi30 <aarohi@Aarohis-MacBook-Pro.local>
2026-03-31 13:20:18 -0700

    commit PrintList changes

--- src/PrintList.java
+++ src/PrintList.java
@@ -5,6 +5,7 @@ public class PrintList {
     public static void main(String[] args) {
         List<String> list = Arrays.asList("apple", "banana", "cherry", "date", "elderberry");
         printListBeforeJava8(list);
+        printListWithJava8(list);
     }
 
     public static void printListBeforeJava8(List<String> list) {
@@ -15,5 +16,6 @@ public class PrintList {
 
     public static void printListWithJava8(List<String> list) {
         list.forEach(System.out::println);
+        //list.forEach(item -> System.out.println(item));
     }
 }
